f75fd100e161aff569684bfeaf7ab0511759ded8
Sahil Shaikh <sahilshaiikh@hotmail.com>
2024-10-02 17:56:59 +0530

    remove static caching - nextjs

--- src/lib/server/utils.ts
+++ src/lib/server/utils.ts
@@ -1,3 +1,4 @@
+import { cookies } from "next/headers";
 import { Client, Databases } from "node-appwrite";
 
 const APPWRITE_ENDPOINT = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || "";
@@ -9,5 +10,7 @@ const client = new Client()
 
 const databases = new Databases(client);
 
-export const getProjects = async () =>
+export const getProjects = async () => {
+  const _ = cookies();
   databases.listDocuments("66fce36200234e0b6158", "66fce37100364fd2637c");
+};
