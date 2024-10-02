162cd4868e71d212f7ed41d00d0e19efed10844b
Sahil Shaikh <sahilshaiikh@hotmail.com>
2024-10-02 19:18:07 +0530

    refactor

--- src/components/sections/hero-section.tsx
+++ src/components/sections/hero-section.tsx
@@ -167,7 +167,7 @@ const BentoGrids = () => {
         <GridOption
           title="Your ultimate all in one Developer"
           subScript=""
-          subTitle="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis maiores iste placeat ipsa sequi laboriosam hic repudiandae nesciunt distinctio autem est, voluptate iusto rem dolores blanditiis vero culpa. Molestias, aliquam reprehenderit, consectetur ducimus quaerat labore dolore ab hic error doloremque cupiditate quasi incidunt quo placeat debitis temporibus tempora, saepe iusto?"
+          subTitle="I am a backend developer with expertise in Node.js. I have experience in building scalable, secure, and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer."
           Link={"/contact"}
           CTA="Get in touch >"
           image=""
